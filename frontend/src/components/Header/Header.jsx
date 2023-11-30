20455e3c14c7bc57dcc9f1603602625536cccf51
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-11-30 20:18:50 +0200

    uploading header and header.css

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  return <div>This is a header</div>;
+  return (
+    <>
+      <header>
+        <div className="info-container">
+          <div className="info-edit">Edit</div>
+          <img
+            className="info-ava"
+            src="https://i.redd.it/0gaywfkq6l071.png"
+            alt=""
+          />
+          <div className="info-username">Khang Nguyen</div>
+          <div className="info-age">21 years old</div>
+          <div className="info-about">I'm a full-stack developer</div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
