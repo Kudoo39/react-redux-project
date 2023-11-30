4e336c6c0b6bef874ba99afa14202c532a2e00f2
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-11-30 21:41:37 +0200

    fixing bug for Edit

--- frontend/src/components/Edit/Edit.jsx
+++ frontend/src/components/Edit/Edit.jsx
@@ -5,18 +5,18 @@ const Edit = () => {
   return (
     <>
       <form>
-        <div className="edit-container">
+        <section className="edit-container">
           <button className="close">Save</button>
-        </div>
-        <div className="edit-profile">Edit Profile</div>
-        <div className="input-container">
-          <label htmlFor="">Display name</label>
-          <input type="text" placeholder="Khang Nguyen" />
-          <label htmlFor="">Age</label>
-          <input type="text" placeholder="21" />
-          <label htmlFor="">About</label>
-          <textarea className="input-about"></textarea>
-        </div>
+          <div className="edit-profile">Edit Profile</div>
+          <div className="input-container">
+            <label htmlFor="">Display name</label>
+            <input type="text" placeholder="Khang Nguyen" />
+            <label htmlFor="">Age</label>
+            <input type="text" placeholder="21" />
+            <label htmlFor="">About</label>
+            <textarea className="input-about"></textarea>
+          </div>
+        </section>
       </form>
     </>
   );
