e0bad503be3c143b21a01bfb46129fcbb5aced04
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-11-30 23:42:35 +0200

    reducers, state and action

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -9,4 +9,13 @@ export const userSlice = createSlice({
     avatar: "https://i.redd.it/0gaywfkq6l071.png",
     theme: "#ccda07",
   },
+  reducers: {
+    update: (state, action) => {
+      state.name = action.payload.name;
+      state.age = action.payload.age;
+      state.about = action.payload.about;
+      state.avatar = action.payload.avatar;
+      state.theme = action.payload.theme;
+    },
+  },
 });
