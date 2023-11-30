b098086adcf0ee0b9282b02b5b7f9941dea0fa13
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-11-30 21:18:49 +0200

    adding edit and edit css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Edit from "./components/Header/Edit/Edit";
 
 const App = () => {
+  const [isEdit, setEdit] = useState(false);
   return (
     <div className="App">
       <Header />
+      <Edit />
     </div>
   );
 };
---
+++ frontend/src/components/Header/Edit/Edit.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Edit.css";
+
+const Edit = () => {
+  return <div>This is edit page</div>;
+};
+
+export default Edit;
