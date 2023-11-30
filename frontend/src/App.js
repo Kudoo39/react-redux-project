1de089021010650ec28d86a3a2e8a5b32166ced6
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-11-30 21:30:49 +0200

    conditional rendering for edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
-import Edit from "./components/Header/Edit/Edit";
+import Edit from "./components/Edit/Edit";
 
 const App = () => {
   const [isEdit, setEdit] = useState(false);
   return (
     <div className="App">
-      <Header setEdit={setEdit} />
-      <Edit />
+      {!isEdit && <Header setEdit={setEdit} />}
+      {isEdit && <Edit />}
     </div>
   );
 };
