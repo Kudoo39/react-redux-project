b4fe8193eee471660c403155dafa5a956161a58b
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-11-30 21:40:35 +0200

    adding more Edit component

--- frontend/src/components/Edit/Edit.jsx
+++ frontend/src/components/Edit/Edit.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import "./Edit.css";
 
 const Edit = () => {
-  return <div>This is edit page</div>;
+  return (
+    <>
+      <form>
+        <div className="edit-container">
+          <button className="close">Save</button>
+        </div>
+        <div className="edit-profile">Edit Profile</div>
+        <div className="input-container">
+          <label htmlFor="">Display name</label>
+          <input type="text" placeholder="Khang Nguyen" />
+          <label htmlFor="">Age</label>
+          <input type="text" placeholder="21" />
+          <label htmlFor="">About</label>
+          <textarea className="input-about"></textarea>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default Edit;
