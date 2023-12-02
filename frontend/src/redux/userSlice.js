7f7394f9a54bbd0cfb2e5d12bc3089ca9fe5a42b
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-02 23:22:12 +0200

    adding updateStart, updateError, updateSuccess for userSlice

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -8,17 +8,34 @@ export const userSlice = createSlice({
     about: "I am a full-stack developer",
     avatar: "https://i.redd.it/0gaywfkq6l071.png",
     theme: "#ccda07",
+    pending: false,
+    error: false,
   },
   reducers: {
-    update: (state, action) => {
+    updateStart: (state) => {
+      state.pending = true;
+    },
+    updateError: (state) => {
+      state.pending = false;
+      state.error = true;
+    },
+    updateSuccess: (state, action) => {
+      state.pending = false;
+      state.error = false;
       state.name = action.payload.name;
       state.age = action.payload.age;
       state.about = action.payload.about;
       state.avatar = action.payload.avatar;
       state.theme = action.payload.theme;
     },
+    // update: (state, action) => {
+    //   state.name = action.payload.name;
+    //   state.age = action.payload.age;
+    //   state.about = action.payload.about;
+    //   state.avatar = action.payload.avatar;
+    //   state.theme = action.payload.theme;
   },
 });
 
-export const { update } = userSlice.actions;
+export const { updateStart, updateError, updateSuccess } = userSlice.actions;
 export default userSlice.reducer;
