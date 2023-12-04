87ffd6ab3366656f1fa0f0638ee947efe933bf5b
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-04 14:47:41 +0200

    finishing the post with styles

--- frontend/src/components/Post/Display.jsx
+++ frontend/src/components/Post/Display.jsx
@@ -4,13 +4,16 @@ import "./Post.css";
 
 const Display = () => {
   const post = useSelector((state) => state.post.posts);
+  const tags = ["None", "NSFW", "Mood", "Quotes", "Shitpost"];
   return (
     <>
       <section className="post-container">
         {post.slice(1).map((post, index) => (
           <div className="posts" key={index}>
             <p className="posts-title">{post.title}</p>
-            <p className="posts-tags">{post.tag}</p>
+            <p className={`posts-tags-${tags[post.tag]} posts-tags`}>
+              {tags[post.tag]}
+            </p>
             <p className="posts-description">{post.description}</p>
           </div>
         ))}
