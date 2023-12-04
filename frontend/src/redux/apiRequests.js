25c1243380686d380d037ec78714ac5c315d21a3
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-04 22:12:57 +0200

    reset everything

--- frontend/src/redux/apiRequests.js
+++ frontend/src/redux/apiRequests.js
@@ -4,10 +4,7 @@ import axios from "axios";
 export const updateUser = async (user, dispatch) => {
   dispatch(updateStart());
   try {
-    const res = await axios.post(
-      "https://react-redux-project-kudoo39.vercel.app/v1/update",
-      user
-    );
+    const res = await axios.post("/v1/update", user);
     dispatch(updateSuccess(res.data));
   } catch (err) {
     dispatch(updateError());
